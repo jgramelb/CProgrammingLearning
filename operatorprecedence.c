0b94e8e3000ebfc58af9faf2849007b6a556c975
jgramelb <jgramelb@users.noreply.github.com>
2018-02-03 08:57:22 -1000

    Add files via upload

---
+++ operatorprecedence.c
@@ -0,0 +1,26 @@
+
+//#include <stdio.h>
+
+/* PRECEDENCE
+Think of order of operations
+
+(),[],-,>,.,++,-- LEFT TO RIGHT
++,-,!,~,++,--(type),*,&,sizeof RIGHT TO LEFT
+*,/,% LEFT TO RIGHT
++,-
+<<,>>
+<,<=,>,=>
+== , !=
+
+BITWISE PRECEDENCE
+&    LEFT TO RIGHT
+^ (Bitwise XOR)
+|
+&&
+||
+?:  RIGHT TO LEFT
+=,+=,-=,*=,/=, %=, >>=, <<=, &=, ^=, |=
+, LEFT TO RIGHT
+
+
+*/
