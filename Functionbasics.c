9a75748b08010c0ebf2d7c14925a97abebad0cc2
jgramelb <jgramelb@users.noreply.github.com>
2018-02-23 12:04:32 -1000

    Create Functionbasics.c

---
+++ Functionbasics.c
@@ -0,0 +1,22 @@
+/* FUNCTIONS
+Function - self contained unit of program, perform tasks - causes action or find program value or status of insformation
+
+Functions need to be small and cohesive 
+-reduces complexity
+-seperate functions are written for each subtask
+
+Functions reduce duplication of code
+Helps with readability 
+
+Programmers think of function as a black box:
+  Info that goes in (its input)
+  The value or action it produces (its output)
+  * Hlps u concentrate on the programs overall design rather than the details of what the func should do & how it relates to the program as a whole before worrying about the code
+
+printf()
+  First argument is usually a string literal, succeeding arguments are a series of variables or expressions whose vlues are to be displayed
+ 
+ scanf() canr eceive info back from function in two ways
+  -Input is stored in an address that you supply as an argument
+  -As a return value
+*/
