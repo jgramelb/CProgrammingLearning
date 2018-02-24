75dc4d0b2624c2972e3587f618f7a2be0d428eb8
jgramelb <jgramelb@users.noreply.github.com>
2018-02-23 14:51:50 -1000

    Update returningdatafromfunc.c

--- returningdatafromfunc.c
+++ returningdatafromfunc.c
@@ -5,3 +5,35 @@ Return_type Function_name(List of parameters - separated by commas)
 STATEMENTS GO HERE
 }
 
+return statement provides the means of exiting from a function 
+-similar to break in loops
+
+psuedocode:
+return expression;
+
+returns the result of that expression
+conversion is possible
+
+int x = myFunctionCall();
+whatever the function returns, will be assigned to x
+
+return values returned from function calls
+
+*/
+
+#include <stdio.h>
+
+int multiplyTwoNumbers(int x, int y)
+{
+  int result = x*y;
+  return result;
+}
+
+int main(void)
+{
+  int result = 0;
+  result = multiplyTwoNubers(10,20);
+  
+  printf("result is %d\n", result);
+  return 0;
+}
